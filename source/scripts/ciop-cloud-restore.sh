26922ce49a455456c6961260244beef4840a95e8
Cesare Rossi <cesare.rossi@terradue.com>
2014-07-16 17:25:20 +0200

    Update ciop-cloud-restore.sh

--- source/scripts/ciop-cloud-restore.sh
+++ source/scripts/ciop-cloud-restore.sh
@@ -9,7 +9,9 @@ mv /tmp/oozie /var/log/
 umount /mnt/application
 rm -rf /application/*
 
-mount /dev/ /application
+app_disk=`blkid | grep CIOP_APP | cut -d: -f1`
+
+mount $app_disk /application
 
 mv /etc/rc.d/rc.local.bkp /etc/rc.d/rc.local
 
