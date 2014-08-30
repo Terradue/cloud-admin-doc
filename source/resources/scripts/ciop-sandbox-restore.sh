afb581a8752da61655db79f88d2849b8e3699ad5
Cesare Rossi <cesare.rossi@terradue.com>
2014-08-30 16:46:16 +0200

    Update and rename ciop-cloud-restore.sh to ciop-sandbox-restore.sh

--- source/resources/scripts/ciop-sandbox-restore.sh
+++ source/resources/scripts/ciop-sandbox-restore.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+# restoring fstab
 mv /etc/fstab.bkp /etc/fstab
 
+# restoring contextualization log
 mv /var/log/context.log.sandbox /var/log/context
 
+# restoring oozie log
 mv /tmp/oozie /var/log/
 
+# restoring application disk
 umount /mnt/application
 rm -rf /application/*
 
 app_disk=`blkid | grep CIOP_APP | cut -d: -f1`
-
 mount $app_disk /application
 
+# restoring rc.local
 mv /etc/rc.d/rc.local.bkp /etc/rc.d/rc.local
 
 exit 0
